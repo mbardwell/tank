1de790bfad55e4337d06ee342e9a511e096f4268
MB <bardwell@ualberta.ca>
2018-07-10 18:18:54 -0600

    Initilising blynk code

---
+++ webpage/lib/passwords/passwords.h
@@ -0,0 +1,16 @@
+#ifndef PASSWORDS_H_
+#define PASSWORDS_H_
+
+// #define home
+#define home
+
+#ifdef lab
+  #define homeSSID "lab"
+  #define homePASS "rettontancep62"
+#endif
+#ifdef home
+  #define homeSSID "PMW"
+  #define homePASS "MmeAllard"
+#endif
+
+#endif
