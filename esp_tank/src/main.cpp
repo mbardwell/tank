6971c50d37f950ab055d94aa09ebdced1eae6e7e
MB <bardwell@ualberta.ca>
2018-07-11 15:39:28 -0600

    Combined multiple examples into esp_tank. Removed broken SPIFFS code base

--- esp_tank/src/main.cpp
+++ esp_tank/src/main.cpp
@@ -1,24 +1,71 @@
+/* Comment this out to disable prints and save space */
+// #define BLYNK_PRINT Serial
 
 
-// Replace with your network credentials
-const char* ssid     = homeSSID;
-const char* password = homePASS;
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <passwords.h>
 
-const int output2 = 2;
-const int output0 = 0;
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = _AUTHTOKEN;
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = _SSID;
+char pass[] = _PASS;
+
+const int PWM1 = 3;
+const int PWM2 = 4;
 
 void setup() {
   Serial.begin(115200);
 
+  pinMode(0, OUTPUT);
+  pinMode(2, OUTPUT);
+
   // Initialize the output variables as outputs
-  pinMode(output0, OUTPUT);
-  pinMode(output2, OUTPUT);
+  pinMode(PWM1, OUTPUT);
+  pinMode(PWM2, OUTPUT);
   analogWriteRange(100);
   analogWriteFreq(1500);
-  analogWrite(output0, 0);
-  analogWrite(output2, 0);
+  analogWrite(PWM1, 0);
+  analogWrite(PWM2, 0);
+
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
 }
 
 void loop(){
+  Blynk.run();
+}
 
+BLYNK_WRITE(V0)
+{
+  int pinValue = param.asInt();
+  if (pinValue > 700) {
+    digitalWrite(0,1);
+  }
+  digitalWrite(0,0);
+}
+BLYNK_WRITE(V1)
+{
+  int pinValue = param.asInt();
+  if (pinValue > 700) {
+    digitalWrite(2,1);
+  }
+  digitalWrite(2,0);
+}
+BLYNK_WRITE(V2)
+{
+  int pinValue = param.asInt();
+}
+BLYNK_WRITE(V3)
+{
+  int pinValue = param.asInt();
+  Serial.print("V1 Slider value is: ");
+  Serial.println(pinValue);
+  digitalWrite(0, pinValue);
 }
